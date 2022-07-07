b82cdacac7b06820ceb80b63a99412b0e8f90698
luciano44 <luciano.ar6@gmail.com>
2022-07-07 02:30:56 -0300

    last task now on top, add task id, change icon, splash screen and overall app style

--- App.js
+++ App.js
@@ -12,13 +12,7 @@ import {
 export default function App() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
-  const [todos, setTodos] = useState([
-    {
-      id: 0,
-      title: "Example task title",
-      description: "Example task description",
-    },
-  ]);
+  const [todos, setTodos] = useState([]);
 
   function deleteTask(id) {
     setTodos(todos.filter((todo) => todo.id !== id));
@@ -28,12 +22,14 @@ export default function App() {
     <View style={styles.container}>
       <Text style={styles.title}>TO DO LIST</Text>
       <View style={styles.newItemContainer}>
+        <Text style={styles.tasksText}>Adicionar Tarefa</Text>
+
         <TextInput
           value={title}
           onChangeText={(text) => {
             setTitle(text);
           }}
-          placeholder={"Title"}
+          placeholder={"Título"}
           style={styles.textinput}
         />
         <TextInput
@@ -41,7 +37,7 @@ export default function App() {
           onChangeText={(text) => {
             setDescription(text);
           }}
-          placeholder={"Description"}
+          placeholder={"Descrição"}
           style={styles.textinput}
         />
 
@@ -49,38 +45,49 @@ export default function App() {
           style={styles.pressable}
           onPress={() => {
             if (title && description) {
+              const id = todos.length > 0 ? todos[0]["id"] + 1 : 1;
               setTodos([
+                {
+                  id,
+                  title,
+                  description,
+                },
                 ...todos,
-                { id: todos.length + 1, title, description },
               ]);
               setTitle("");
               setDescription("");
             } else {
-              alert("⚠ You must fill all the fields! ");
+              alert(
+                "👉 Preencha todos os campos para adicionar uma nova tarefa."
+              );
             }
           }}
         >
-          <Text style={styles.pressableText}>Add Task</Text>
+          <Text style={styles.pressableText}>Adicionar Tarefa</Text>
         </Pressable>
+        <Text style={styles.tasksText}>Tarefas</Text>
       </View>
-      <Text style={styles.tip}>
-        Tap the tasks to erase as you complete them!
-      </Text>
+
       <ScrollView style={styles.items}>
-        {todos.map((todo, i) => {
+        {todos.map((todo) => {
           return (
             <Pressable
-              key={i}
+              key={todo.id}
               onPress={() => {
                 deleteTask(todo.id);
               }}
               style={styles.task}
             >
-              <Text style={styles.itemTitle}>{todo.title}</Text>
+              <Text style={styles.itemTitle}>
+                #{todo.id} <Text style={styles.titleBold}>{todo.title}</Text>
+              </Text>
               <Text style={styles.itemDesc}>{todo.description}</Text>
             </Pressable>
           );
         })}
+        {todos.length < 1 && (
+          <Text style={styles.noTasks}>{`Não há tarefas :(`}</Text>
+        )}
       </ScrollView>
       <StatusBar backgroundColor="#f9f9f9" />
     </View>
@@ -95,24 +102,39 @@ const styles = StyleSheet.create({
   task: {
     backgroundColor: "#fff",
     margin: 5,
-    borderRadius: 8,
+    borderRadius: 5,
     padding: 10,
-    shadowColor: "#000",
+    shadowColor: "black",
     shadowOffset: {
       width: 0,
       height: 2,
     },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
-
     elevation: 5,
+  },
+  tasksText: {
+    fontWeight: "bold",
+    fontSize: 20,
+    color: "dodgerblue",
+    margin: 20,
+  },
+  noTasks: {
+    color: "gray",
+    marginTop: "50%",
+    fontSize: 25,
+    fontWeight: "bold",
+    textAlign: "center",
   },
   itemTitle: {
     color: "#555",
-    borderBottomColor: "#bbb",
+    borderBottomColor: "dodgerblue",
     borderBottomWidth: 1,
     fontSize: 18,
     padding: 8,
+  },
+  titleBold: {
+    fontWeight: "bold",
   },
   itemDesc: {
     color: "#999",
@@ -120,7 +142,7 @@ const styles = StyleSheet.create({
     padding: 8,
   },
   title: {
-    backgroundColor: "#242C88",
+    backgroundColor: "dodgerblue",
     color: "#eadff5",
     textAlign: "center",
     fontSize: 20,
@@ -133,28 +155,32 @@ const styles = StyleSheet.create({
     paddingRight: 10,
     width: "100%",
   },
-  NewItemContainer: {
-    borderWidth: 1,
-    borderColor: "#bbb",
-  },
+  newItemContainer: {},
   textinput: {
     backgroundColor: "#fff",
     padding: 10,
     margin: 4,
-    width: "95%",
+    width: "90%",
     marginLeft: "auto",
     marginRight: "auto",
-    borderColor: "#ddd",
-    borderWidth: 1,
+    borderRadius: 10,
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
   pressable: {
-    backgroundColor: "#242C88",
-    width: "80%",
+    backgroundColor: "dodgerblue",
+    width: "90%",
     marginLeft: "auto",
     marginRight: "auto",
     marginTop: 5,
-    borderRadius: 10,
     padding: 10,
+    borderRadius: 10,
   },
   pressableText: {
     color: "#fff",
@@ -165,6 +191,8 @@ const styles = StyleSheet.create({
     color: "gray",
     backgroundColor: "transparent",
     textAlign: "center",
-    padding: 15,
+    fontSize: 13,
+    color: "green",
+    padding: 3,
   },
 });
